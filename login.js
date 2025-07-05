86a0ba36f07c8ed5ddc73c17048e76399dc0c8e2
Abhishek Chand <0101abhi0101@gmail.com>
2025-07-05 14:26:49 +0530

    Initial commit

---
+++ login.js
@@ -0,0 +1,19 @@
+document.getElementById('login-form').addEventListener('submit', async function (e) {
+    e.preventDefault();
+
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
+
+    const response = await fetch('https://notes-api-c1mt.onrender.com/api/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+    });
+
+    const data = await response.json();
+
+    const msg = document.getElementById('msg');
+    msg.innerText = data.message;
+});
